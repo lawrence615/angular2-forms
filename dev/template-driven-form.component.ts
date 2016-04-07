db8af40c64fd6a5f7092ac1a40cd3cf28dfb3b0e
Lawrence Macharia <macharialawrence2011@gmail.com>
2016-04-07 05:42:53 +0300

    initial commit

---
+++ dev/template-driven-form.component.ts
@@ -0,0 +1,13 @@
+import {Component} from "angular2/core";
+/**
+ * Created by Lawrence on 4/7/16.
+ */
+@Component({
+    selector: 'template-driven-form',
+    template: `
+    `
+})
+
+export class TemplateDrivenFormComponent {
+
+}
