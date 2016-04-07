d6759b663c7a6d67b002fae610e218621907d1d0
Lawrence Macharia <macharialawrence2011@gmail.com>
2016-04-07 06:20:14 +0300

    initial commit
    This component has a form; when submitted we get the values and display
    them in the console

--- dev/template-driven-form.component.ts
+++ dev/template-driven-form.component.ts
@@ -5,9 +5,28 @@ import {Component} from "angular2/core";
 @Component({
     selector: 'template-driven-form',
     template: `
+        <h2>Sign-up Form</h2>
+        <form (ngSubmit)="onSubmit(f)" #f="ngForm">
+            <div>
+                <label for="mail">Mail</label>
+                <input ngControl="mail" type="text" id="mail" required/>
+            </div>
+            <div>
+                <label for="password">Password</label>
+                <input ngControl="password" type="text" id="password" required/>
+            </div>
+            <div>
+                <label for="confirm_password">Confirm Password</label>
+                <input ngControl="confirm_password"  type="text" id="confirm_password" required/>
+            </div>
+            <button type="submit">Submit </button>
+        </form>
     `
 })
 
 export class TemplateDrivenFormComponent {
 
+    onSubmit(form) {
+        console.log(form.value);
+    }
 }
