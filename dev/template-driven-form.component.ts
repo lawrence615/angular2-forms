4bcfa913afb1d7ec956d8b596c80ad7c306f326d
Lawrence Macharia <macharialawrence2011@gmail.com>
2016-04-09 15:24:28 +0300

    Added validation to the form and retrieved the submitted data displaying it in a div

--- dev/template-driven-form.component.ts
+++ dev/template-driven-form.component.ts
@@ -9,24 +9,33 @@ import {Component} from "angular2/core";
         <form (ngSubmit)="onSubmit(f)" #f="ngForm">
             <div>
                 <label for="mail">Mail</label>
-                <input ngControl="mail" type="text" id="mail" required/>
+                <input ngControl="mail" type="text" id="mail" required #email="ngForm"/>
+                <span class="validation-error" *ngIf="!email.valid">Not Valid</span>
             </div>
             <div>
                 <label for="password">Password</label>
-                <input ngControl="password" type="text" id="password" required/>
+                <input ngControl="password" type="text" id="password" required  #password="ngForm"/>
+                 <span class="validation-error" *ngIf="!password.valid">Not Valid</span>
             </div>
             <div>
                 <label for="confirm_password">Confirm Password</label>
-                <input ngControl="confirm_password"  type="text" id="confirm_password" required/>
+                <input ngControl="confirm_password"  type="text" id="confirm_password" required  #confirm_password="ngForm"/>
+                 <span class="validation-error" *ngIf="!confirm_password.valid">Not Valid</span>
             </div>
-            <button type="submit">Submit </button>
+            <button type="submit" [disabled]="!f.valid ||password.value !== confirm_password.value ">Submit </button>
         </form>
+        <h2>You submitted:</h2>
+        <div>Mail: {{user.mail}}</div>
+        <div>Password: {{user.pass}}</div>
     `
 })
 
 export class TemplateDrivenFormComponent {
+    user = {'mail': '', 'pass': ''}
 
     onSubmit(form) {
-        console.log(form.value);
+        //console.log(form.value);
+        this.user.mail = form.value['mail']
+        this.user.pass = form.controls['password'].value
     }
 }
